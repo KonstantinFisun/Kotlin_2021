5513a38790f2948a3ad456052076d26cf291d0b6
KonstantinFisun <72182894+KonstantinFisun@users.noreply.github.com>
2021-04-13 16:17:52 +0300

    ЛБ 2 Задание 2

--- Laba_2/src/Main.kt
+++ Laba_2/src/Main.kt
@@ -4,6 +4,10 @@ import java.util.*
 //Суммы элементов рекурсия вверх
 fun sumElemUp(num:Int):Int = if(num==0) 0 else (num%10)+sumElemUp(num/10)
 
+//Сумма элементов рекурсия вниз
+//Для пользователя
+fun sumElemDown(num:Int) = sumElemDown(num,0)
+tailrec fun sumElemDown(num:Int,sum:Int):Int = if(num>0) sumElemDown(num/10,sum+(num%10)) else sum
 
 fun main() {
 
@@ -12,6 +16,7 @@ fun main() {
     val s:Int = scanner.nextInt()
 
     println("Сумма элементов рекурсия вверх ${sumElemUp(s)}")
-
+    println("Сумма элементов рекурсия вниз ${sumElemDown(s)}")
+    
 }
 
