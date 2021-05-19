7273fbe695b2c687a233a95df1a3a10073dae3e1
KonstantinFisun <72182894+KonstantinFisun@users.noreply.github.com>
2021-05-19 21:53:22 +0300

    Задание 2
    Реализовать и протестировать вывод документа на экран.

--- Laba_5/src/Main.kt
+++ Laba_5/src/Main.kt
@@ -1,3 +1,5 @@
 fun main() {
-
+    val passTS = TransportVehicle("XTZ131HA00D024175","ЗИЛ 131-HA",
+        "БОРТОВОЙ ГРУЗОВОЙ",1991,"1012605003256263613", "Павленко Лилия Станисловна")
+    passTS.writePas()
 }
--- Laba_5/src/TransportVehicle.kt
+++ Laba_5/src/TransportVehicle.kt
@@ -2,9 +2,18 @@
 class TransportVehicle(var vin : String, var markaModel : String, var type : String,
                        var yearOfManufacture : Int, var series : String,
                        var fio : String)
-
 {
-
+    //Функция вывода на экран документа
+    fun writePas(){
+        println("Паспорт транспортного средства.\n" +
+                "VIN : $vin\n" +
+                "Марка, модель ТС : $markaModel\n" +
+                "Тип ТС : $type\n" +
+                "Год изготовления ТС : $yearOfManufacture\n" +
+                "Серия ТС : $series\n" +
+                "ФИО владельца ТС : $fio\n"
+        )
+    }
 
 
 
