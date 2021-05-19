9b2c2ff9ae42ce068a8fa1a741187df638f3b30f
KonstantinFisun <72182894+KonstantinFisun@users.noreply.github.com>
2021-05-19 21:41:46 +0300

    Задание 1.
    Создать класс, содержащий информацию о документе

---
+++ Laba_5/src/TransportVehicle.kt
@@ -0,0 +1,11 @@
+//Паспорт траспортного средства
+class TransportVehicle(var vin : String, var markaModel : String, var type : String,
+                       var yearOfManufacture : Int, var series : String,
+                       var fio : String)
+
+{
+
+
+
+
+}
