077516a1f633ff41f669b3186e7002f049694d6e
KonstantinFisun <72182894+KonstantinFisun@users.noreply.github.com>
2021-05-19 23:53:23 +0300

    Задание 3
    Реализовать сравнение документов по серии, номеру.

--- Laba_5/src/Main.kt
+++ Laba_5/src/Main.kt
@@ -1,5 +1,10 @@
 fun main() {
     val passTS = TransportVehicle("XTZ131HA00D024175","ЗИЛ 131-HA",
         "БОРТОВОЙ ГРУЗОВОЙ",1991,"1012605003256263613", "Павленко Лилия Станисловна")
-    passTS.writePas()
+    val passTS1 = TransportVehicle("XTZ131HA00D024175","ЗИЛ 131-HA",
+        "БОРТОВОЙ ГРУЗОВОЙ",1991,"1012615003256263613", "Павленко Лилия Станисловна")
+    if(passTS<passTS1)
+        passTS.writePas()
+    else
+        passTS1.writePas()
 }
--- Laba_5/src/TransportVehicle.kt
+++ Laba_5/src/TransportVehicle.kt
@@ -1,7 +1,7 @@
 //Паспорт траспортного средства
 class TransportVehicle(var vin : String, var markaModel : String, var type : String,
                        var yearOfManufacture : Int, var series : String,
-                       var fio : String)
+                       var fio : String)  : Comparable<TransportVehicle>
 {
     //Функция вывода на экран документа
     fun writePas(){
@@ -15,6 +15,28 @@ class TransportVehicle(var vin : String, var markaModel : String, var type : Str
         )
     }
 
+    //Переопределение операции сравнения
+    override fun equals(other: Any?): Boolean {
+        return if (other is TransportVehicle) {
+            series == other.series
+        } else
+            false
+    }
+
+    override fun compareTo(other: TransportVehicle ): Int {
+        return compareValuesBy(this , other ,
+            TransportVehicle :: series, TransportVehicle :: vin) //Сначала сравниваем по серии, потом по вин
+    }
+
+    override fun hashCode(): Int {
+        var result = vin.hashCode()
+        result = 31 * result + markaModel.hashCode()
+        result = 31 * result + type.hashCode()
+        result = 31 * result + yearOfManufacture
+        result = 31 * result + series.hashCode()
+        result = 31 * result + fio.hashCode()
+        return result
+    }
 
 
 }
