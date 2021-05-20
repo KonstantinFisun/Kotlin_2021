7796036ff4271d940df23a4377655dc77f625e87
KonstantinFisun <72182894+KonstantinFisun@users.noreply.github.com>
2021-05-20 21:33:54 +0300

    Задание 4.
     Реализовать валидацию полей класса с помощью регулярных 
    выражений.

--- Laba_5/src/Main.kt
+++ Laba_5/src/Main.kt
@@ -1,10 +1,7 @@
 fun main() {
-    val passTS = TransportVehicle("XTZ131HA00D024175","ЗИЛ 131-HA",
+    val passTS = TransportVehicle("XTZ131HA00D024795","ЗИЛ 131-НА",
         "БОРТОВОЙ ГРУЗОВОЙ",1991,"1012605003256263613", "Павленко Лилия Станисловна")
-    val passTS1 = TransportVehicle("XTZ131HA00D024175","ЗИЛ 131-HA",
-        "БОРТОВОЙ ГРУЗОВОЙ",1991,"1012615003256263613", "Павленко Лилия Станисловна")
-    if(passTS<passTS1)
-        passTS.writePas()
-    else
-        passTS1.writePas()
+
+    passTS.validation()
+
 }
--- Laba_5/src/TransportVehicle.kt
+++ Laba_5/src/TransportVehicle.kt
@@ -1,7 +1,7 @@
 //Паспорт траспортного средства
-class TransportVehicle(var vin : String, var markaModel : String, var type : String,
-                       var yearOfManufacture : Int, var series : String,
-                       var fio : String)  : Comparable<TransportVehicle>
+class TransportVehicle(var vin : String = "", var markaModel : String = "", var type : String = "",
+                       var yearOfManufacture : Int = 0, var series : String,
+                       var fio : String = "")  : Comparable<TransportVehicle>
 {
     //Функция вывода на экран документа
     fun writePas(){
@@ -36,6 +36,77 @@ class TransportVehicle(var vin : String, var markaModel : String, var type : Str
         result = 31 * result + series.hashCode()
         result = 31 * result + fio.hashCode()
         return result
+    }
+
+    //VIN - 17 букв или цифр, где последнии 4 являются цифрами
+    //Марка модель - цифра в конце
+    //Тип - без цифр
+    //Год выпуска - 4 цифры
+    //Серия - только цифр
+    //Фамилия Имя Отчество - 3 слова без цифр
+
+    //Функция проверки правильности ввода данных(валидация)
+    fun validation(){
+        checkVIN()
+        checkmarkaModel()
+        checkType()
+        checkSeries()
+        checkYearOfManufacture()
+        checkFIO()
+    }
+
+    //Проверка vin
+    fun checkVIN() : Boolean{
+        if(vin.length != 17) {
+            println("Количество букв не совпадает в vin : ${vin.length}")
+            return false
+        }
+        val matchResult = Regex("""(\w{13})([0-9]{4})""").find(vin)
+        if(matchResult == null) {
+            println("Введен неправильный вин")
+            return false
+        }
+        return true
+    }
+
+    fun checkmarkaModel() : Boolean{
+        if(markaModel.matches(Regex("""[а-яА-Яa-zA-Z\s0-9\-]{6,25}"""))) {
+            return true
+        }
+        println("Введена неправильная марка и модель авто")
+        return false
+    }
+
+    fun checkSeries() : Boolean{
+        if(series.matches(Regex("""[0-9]+"""))) {
+            return true
+        }
+        println("Введена неправильная серия")
+        return false
+    }
+
+    fun checkYearOfManufacture() : Boolean{
+        if(yearOfManufacture>1900 && yearOfManufacture<2021){
+            return true
+        }
+        println("Введён некорректный год выпуска")
+        return false
+    }
+
+    fun checkFIO() : Boolean{
+        if(fio.matches(Regex("""\s?([А-ЯA-Z])[а-яa-z]+\s+([А-ЯA-Z])[а-яa-z]+\s+([А-ЯA-Z])[а-яa-z]+\s?"""))) {
+            return true
+        }
+        println("Введена неправильная ФИО")
+        return false
+    }
+
+    fun checkType() : Boolean{
+        if(type.matches(Regex("""[а-яА-Яa-zA-Z\s]+"""))) {
+            return true
+        }
+        println("Введён неправильный тип ТС")
+        return false
     }
 
 
