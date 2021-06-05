6d66137f8cbebf407fbc08a7d0166e9793f94009
KonstantinFisun <72182894+KonstantinFisun@users.noreply.github.com>
2021-06-05 23:45:45 +0300

    ЛБ 4 Задание 4.2
    1.2 Дана строка. Необходимо найти все строчные символы латиницы, 
    которые в ней используются.

--- Laba_4/src/Main.kt
+++ Laba_4/src/Main.kt
@@ -1,6 +1,7 @@
 import java.lang.System.`in`
 import java.util.*
 import kotlin.math.pow
+import kotlin.test.assertEquals
 
 //Задание 1. Дана строка в которой числа перечислены через пробел.
 //Необходимо найти максимальное из этих чисел.
@@ -70,7 +71,46 @@ fun pathFile(str : String) : String {
         .dropLast(str.drop(str.length-str.reversed().indexOf('/')).indexOf(".")+1)
 }
 
+//---------------------------------------------------------------------------------------------------------
 
+
+//Задание 3. Дана строка. Необходимо найти все даты, которые описаны в
+//виде "31/02/2007"
+
+//Для пользователя
+//Идея : проверяем первое вхождение
+//fun date(str : String, index : Int){
+//    if()
+//}
+//
+//fun validDate(str : String){
+//
+//
+//    val reg = "(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\\d\\d".toRegex()
+//    val matchResult = reg.findAll(str, 0)
+//    assertEquals(IntRange(0, 3), matchResult.range())
+//
+//}
+
+//Задание 4:
+//1.2 Дана строка. Необходимо найти все строчные символы латиницы,
+//которые в ней используются.
+
+//Для пользователя
+fun allLowerSimbolLatin(str : String) : MutableList<Char> = run { allLowerSimbolLatin(str, str.length-1, mutableListOf<Char>()) }
+
+tailrec fun allLowerSimbolLatin(str : String, index : Int, useSimbol : MutableList<Char>) : MutableList<Char> {
+    return if(index <= 0) useSimbol
+    else if(str[index] in 'a'..'z' && useSimbol.any{it == str[index]}) allLowerSimbolLatin(str, index - 1, useSimbol)
+    else{
+        if(str[index] in 'a'..'z'){
+            useSimbol.add(str[index])
+            allLowerSimbolLatin(str, index - 1, useSimbol)
+        } else{
+            allLowerSimbolLatin(str, index - 1, useSimbol)
+        }
+    }
+}
 
 fun selectUser(){
     val scanner = Scanner(System.`in`)
@@ -80,7 +120,10 @@ fun selectUser(){
                 "1. Максимальное число в строке.\n" +
                 "2. Упорядочены ли латинские символы.\n" +
                 "3. Количество букв А.\n" +
-                "4. Имя файла")
+                "4. Имя файла.\n" +
+                "5. Найти дату в строке.\n" +
+                "6. Все строчные символы латиницы.\n" +
+                "")
 
         val select = scanner.next()
         if(select == "Exit") { return }
@@ -89,6 +132,8 @@ fun selectUser(){
             "2" -> println(orderedCharacters("zvvs sv s vs fg",'z',13))
             "3" -> println(countA("zvAs sv s As fg",0,13))
             "4" -> println(pathFile("c:/sf/cs/vot.txt"))
+            //"5" -> validDate("fs v 31/02/2007 fs vl 32/02/2003")
+            "6" -> println(allLowerSimbolLatin("fs A 31F2g/2007 fh vl 32/02/2003"))
 
         }
         println("Введите любой символ для продолжения... : ")
