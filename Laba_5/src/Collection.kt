e4179a5069b1292776e8041df97e9bfdb1cdfe71
KonstantinFisun <72182894+KonstantinFisun@users.noreply.github.com>
2021-06-06 11:31:01 +0300

    ЛБ 5 Задание 6

---
+++ Laba_5/src/Collection.kt
@@ -0,0 +1,5 @@
+//Реализовать абстрактный класс коллекция документов с
+//абстрактным методом
+abstract class Collection {
+    abstract fun searchDoc(doc : TransportVehicle) : TransportVehicle
+}
--- Laba_5/src/Main.kt
+++ Laba_5/src/Main.kt
@@ -1,7 +1,9 @@
 fun main() {
-    val passTS = TransportVehicle("XTZ131HA00D024795","ЗИЛ 131-НА",
+
+
+
+    val passTS = TransportVehicle("XTZ131HA00D044795","ЗИЛ 131-НА",
         "БОРТОВОЙ ГРУЗОВОЙ",1991,"1012605003256263613", "Павленко Лилия Станисловна")
 
     passTS.validation()
-
 }
